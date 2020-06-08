95479f8a3941351746d9009784f9eb5ec3e63360
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-06-08 12:00:53 -0400

    comments added

---
+++ algorithms.py
@@ -0,0 +1,27 @@
+#Binary search 
+
+def binary_search(list, item):
+    # low and high keep track of which part of the list you'll search in.
+    low = 0
+    high = len(list) - 1
+
+    # While you haven't narrowed it down to one element
+    while low <= high:
+
+        # ... check the middle element
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))
+print(binary_search(my_list, -3))
+
+
