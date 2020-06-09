680d99e554aa04f0a96ebc3ddc13973f19f6ca34
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-06-09 12:13:19 -0400

    classes added

--- algorithms.py
+++ algorithms.py
@@ -11,6 +11,7 @@ def binary_search(list, item):
         # ... check the middle element
         mid = (low + high) // 2
         guess = list[mid]
+        print(list[mid])
         if guess == item:
             return mid
         if guess > item:
@@ -23,5 +24,4 @@ my_list = [1, 3, 5, 7, 9]
 
 print(binary_search(my_list, 3))
 print(binary_search(my_list, -3))
-
 
